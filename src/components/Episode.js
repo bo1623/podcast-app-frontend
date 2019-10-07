a68f51c3281034210bff8005705c6fda33f6c43f
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-10-07 17:53:09 +0800

    More edits on Episode component

--- src/components/Episode.js
+++ src/components/Episode.js
@@ -8,6 +8,9 @@ export default class Episode extends Component {
     return(
       <div className="episode">
         <p>{episode.title}</p>
+        <p>{episode.description}</p>
+        <p>Published: {episode.published_date}</p>
+        <p>Duration: {episode.audio_length}</p>
       </div>
     )
   }
