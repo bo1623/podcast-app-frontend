9a31c9cbc23de7ff63762acc163535bdcac7773a
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-10-07 21:22:05 +0800

    Formatted date for podcast episodes

--- src/components/Episode.js
+++ src/components/Episode.js
@@ -7,13 +7,20 @@ export default class Episode extends Component {
   render(){
     const {episode} = this.props
     const description=episode.description
+
+    //formatting url
     const url_split=episode.audio_url.split('/e/')
     const new_url=url_split[0]+'/embedded/e/'+url_split[1]
+
+    const months=['January','February','March','April','May','June','July','August','September','October','November','December']
+    const d=new Date(episode.published_date)
+    const datestring= d.getDate()+' '+months[d.getMonth()]+' '+d.getFullYear()
+  
     return(
       <div className="episode">
         <div>{episode.title} <button>Add to Playlist</button> </div>
         <Markup content={description}/>
-        <p>Published: {episode.published_date}</p>
+        <p>Published: {datestring}</p>
         <p>Duration: {episode.audio_length}</p>
         <iframe src={new_url} height="300px" width="100%"></iframe>
       </div>
@@ -21,4 +28,3 @@ export default class Episode extends Component {
   }
 
 }
-// <p>{episode.description}</p>
