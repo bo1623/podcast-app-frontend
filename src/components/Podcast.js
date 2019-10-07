d553d0bba3106a7be23031436d0e109068729e58
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-10-07 12:30:18 +0800

    Modified podcast component to include relevant details

--- src/components/Podcast.js
+++ src/components/Podcast.js
@@ -3,10 +3,17 @@ import React,{Component} from 'react';
 class Podcast extends Component{
 
   render(){
+    const {podcast} = this.props
+
     return(
       <div className='podcast'>
-        <img src={this.props.podcast.title} />
-        
+        <img src={podcast.image} />
+        <div className='podcast-details'>
+          <span>{podcast.title}</span>
+          <br></br>
+          <span>Total Episodes: {podcast.total_episodes}</span>
+        </div>
+        <br></br>
       </div>
     )
   }
