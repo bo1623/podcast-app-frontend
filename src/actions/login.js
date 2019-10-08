6a2f12023ff3c8aaee8943f2f44bc0837f9129ee
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-10-08 17:31:34 +0800

    Installed terser package

--- src/actions/login.js
+++ src/actions/login.js
@@ -1,6 +1,16 @@
+import userUrl from '../fetchUrl'
+
 const login = (username) =>{
   return dispatch =>{
     dispatch({type: "LOG_IN",username:username})
+    fetch(userUrl,{
+      method:'POST',
+      headers: {
+        "Content-Type":"application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(username)
+    })
   }
 }
 
